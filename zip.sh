7e6c9b3e771a1c344079f269d1ccc577e27c6262
Dylan Kenneth Ryals <dryals@login03.negishi.rcac.purdue.edu>
2024-05-03 12:01:52 -0400

    downloading fastq

--- zip.sh
+++ zip.sh
@@ -5,14 +5,14 @@
 #SBATCH -A bharpur
 #SBATCH --nodes=1 
 #SBATCH --ntasks=4
-#SBATCH --time=12:00:00
+#SBATCH --time=1-00:00:00
 #SBATCH --job-name zip
 #SBATCH --output=/home/dryals/ryals/ahb/outputs/zip.out
 #SBATCH --error=/home/dryals/ryals/ahb/outputs/zip.out
 
-cd $CLUSTER_SCRATCH/ahb
+cd $CLUSTER_SCRATCH/ahb/sra
 
-tar -czvf ryals-fikere-impute-fastq.tar sra
+tar -czvf ../ryals-fikere-impute-fastq.tar *
 
 echo "DONE"
 
