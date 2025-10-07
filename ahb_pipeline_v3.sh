70893bf2931aafb45edc3781ec5cd9e529332005
Dylan Ryals <dylan.k.ryals@gmail.com>
2025-10-07 11:06:44 -0400

    Revisions

--- ahb_pipeline_v3.sh
+++ ahb_pipeline_v3.sh
@@ -205,44 +205,43 @@ echo "Analysis"
 echo "---------------------"
     
     
-# echo "starting admix..."
-#     cd /scratch/bell/dryals/ahb
-#     mkdir -p admix
-#     cd admix
-#     mkdir -p unsupervised
-#     mkdir -p supervised
-#   
-#     #supervised
-#         #create pop file
-#         cd /home/dryals/ryals/ahb
-#         R --vanilla --no-save --no-echo --silent < makeAdmixPop.R
-#         sleep 5
-#         sbatch supervised_admix_v3.sh
-#     
-# echo "plink: generating PCA..."
-#     cd $CLUSTER_SCRATCH/ahb
-#     plink --bcf samples.filter.${version}.bcf.gz --make-bed --allow-extra-chr --chr-set 16 no-xy -chr $chrsShort --set-missing-var-ids @:# --threads $SLURM_NTASKS --silent --maf 0.05 --pca 500 --out plink/samps.${version}
-#     
-#     plink --bcf admix.${version}.bcf.gz --make-bed --allow-extra-chr --chr-set 16 no-xy -chr $chrsShort --set-missing-var-ids @:# --silent --threads $SLURM_NTASKS --maf 0.05 --pca 500 --out plink/all.${version}
-#     
-# 
-# echo "starting reference admix..."
-# 
-#     #WARNING: this will break if an admix file already exisit for the version
-#     cd $CLUSTER_SCRATCH/admix/supervised
-#     while [ ! -f "admix.${version}.4.Q" ] \
-#     do
-#         sleep 10 #wait between each check
-#     done
-#     
-#     #overwrite baseneame
-#     cd $CLUSTER_SCRATCH/ahb/plink
-#     echo "reference.${version}" > plink_admix_filename.txt
-#     #reset log file
-#     cd /home/dryals/ryals/ahb
-#     echo -n "" > outputs/usadmix.out
-#     #launch the admixture array
-#     sbatch --array=2-9 unsupervised_v3.sh
+echo "starting admix..."
+    cd /scratch/bell/dryals/ahb
+    mkdir -p admix
+    cd admix
+    mkdir -p unsupervised
+    mkdir -p supervised
+  
+    #supervised
+        #create pop file
+        cd /home/dryals/ryals/ahb
+        R --vanilla --no-save --no-echo --silent < makeAdmixPop.R
+        sleep 5
+        sbatch supervised_admix_v3.sh
+    
+echo "plink: generating PCA..."
+    cd $CLUSTER_SCRATCH/ahb
+    plink --bcf samples.filter.${version}.bcf.gz --make-bed --allow-extra-chr --chr-set 16 no-xy -chr $chrsShort --set-missing-var-ids @:# --threads $SLURM_NTASKS --silent --maf 0.05 --pca 500 --out plink/samps.${version}
+    
+    plink --bcf admix.${version}.bcf.gz --make-bed --allow-extra-chr --chr-set 16 no-xy -chr $chrsShort --set-missing-var-ids @:# --silent --threads $SLURM_NTASKS --maf 0.05 --pca 500 --out plink/all.${version}
+
+echo "starting reference admix..."
+
+    #WARNING: this will break if an admix file already exisit for the version
+    cd $CLUSTER_SCRATCH/admix/supervised
+    while [ ! -f "admix.${version}.4.Q" ] \
+    do
+        sleep 10 #wait between each check
+    done
+    
+    #overwrite baseneame
+    cd $CLUSTER_SCRATCH/ahb/plink
+    echo "reference.${version}" > plink_admix_filename.txt
+    #reset log file
+    cd /home/dryals/ryals/ahb
+    echo -n "" > outputs/usadmix.out
+    #launch the admixture array
+    sbatch --array=2-9 unsupervised_v3.sh
 #     
 #ending output
 echo "---------------------"
